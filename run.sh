e39fddced5a6c30344e3ea5a272ac1714ba22cd5
volt <167736591+vvvooolt@users.noreply.github.com>
2025-10-04 02:50:03 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 /home/volt/Desktop/nasa/testing/search-card-deck/backend/api.py &
+python3 ~/Desktop/nasa/testing/search-card-deck/backend/api.py &
 ollama serve &
 cloudflared tunnel run AI &
 cloudflared tunnel run ollama &
