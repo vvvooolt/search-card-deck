e9279a7f996cf5e2efc15e21d15aa33511280a3f
vvvooolt <randomnameor@gmail.com>
2025-10-04 01:05:24 -0500

    fixed if statement not working for detection of running processes

--- electron-client/order66.sh
+++ electron-client/order66.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 original=$(pwd)
-if $(curl http://localhost:8080/) | grep -q '^curl'; then
+
+# Fetch the response from curl
+response=$(curl -s http://localhost:8080/)
+
+# Check if the response contains the string 'curl'
+if [[ "$response" == *"curl"* ]]; then
+  echo "Found 'curl' in the response!"
   cd ~/Downloads/Keeko/ && ~/Downloads/Keeko/run.sh &
 fi
-sleep 1 && cd  ~/Downloads/Keeko/electron-client && npm start
+
+sleep 1 && cd ~/Downloads/Keeko/electron-client && npm start
 cd $original
+
