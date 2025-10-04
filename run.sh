254f746e507112d8391608a195df45957a03480c
vvvooolt <randomnameor@gmail.com>
2025-10-03 23:03:31 -0500

    remove cloudflare to stick to localhost

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 python3 ~/Desktop/nasa/testing/search-card-deck/backend/api.py &
 ollama serve &
-cloudflared tunnel run AI &
-cloudflared tunnel run ollama &
 npm run dev &
 detach
 wait
