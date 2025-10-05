e5dc6f7e354e951e79c7333089c445edb5fce3e9
Husam Alhayek <eng.termnh@gmail.com>
2025-10-05 15:22:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ echo "Installing Desktop client..."
 sudo chmod +x ./electron-client/install.sh
 ./electron-client/install.sh
 
-sudo pacman -S python-fastapi python-uvicorn
+sudo pacman -S python-fastapi uvicorn python-python-multipart
 
 echo ""
 echo "Installing npm dependencies..."
