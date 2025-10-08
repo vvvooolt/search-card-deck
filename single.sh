c810b4cf8f5f912925042221ca3dd35d44fd434e
volt <randomnameor@gmail.com>
2025-10-08 19:26:50 +0300

    fixed process handling if our server has multiple processes

--- single.sh
+++ single.sh
@@ -3,9 +3,14 @@
 PORTS="8080 3414"
 
 for PORT in $PORTS; do
-  pid=$(lsof -t -i :"$PORT")
-  if [ -n "$pid" ]; then
-    echo "Killing process on port $PORT (PID: $pid)"
-    kill -9 "$pid"
+  # Get the list of PIDs on the specified port
+  pids=$(lsof -t -i :"$PORT")
+  
+  if [ -n "$pids" ]; then
+    for pid in $pids; do
+      echo "Killing process on port $PORT (PID: $pid)"
+      kill -9 "$pid"
+    done
   fi
 done
+
